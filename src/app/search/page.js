bc9b32a5f3fd8cfd4506b33e4117efac3137821b
Elisassa <elisa122.tw@gmail.com>
2025-09-19 16:34:03 -0400

    Add search and history pages

---
+++ src/app/search/page.js
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+import styles from "../globals.module.css";
+
+export default function Search() {
+  return (
+    <>
+      <Navbar />
+      <div className={styles.background}>
+      </div>
+    </>
+  );
+}
